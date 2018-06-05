efc6d6f44606be33b70e264f780f28dda2673966
boyonger <32034024+boyonger@users.noreply.github.com>
2018-06-06 00:09:26 +0800

    maxSkipNumber

---
+++ src/skipNumber/SkipNumber.java
@@ -0,0 +1,44 @@
+package skipNumber;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SkipNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        int maxSumBySkip = 1001;
+        int maxSumNotSkip=0;
+        while ((str = br.readLine()) != null) {
+            Integer count = Integer.parseInt(str);
+            int[] increaseNumber = new int[count];
+            str = br.readLine();
+            String[] number = str.split(" ");
+            for (int i = 0; i < count; i++) {
+                increaseNumber[i] = Integer.parseInt(number[i]);
+            }
+            for (int i=1;i<count;i++){
+                int mid=increaseNumber[i]-increaseNumber[i-1];
+                if (maxSumNotSkip<mid){
+                    maxSumNotSkip=mid;
+                }
+            }
+            boolean flag=false;
+            for (int i = 2; i < count; i++) {
+                int midSkip=(increaseNumber[i] - increaseNumber[i - 2]);
+                if (midSkip<=maxSumNotSkip){
+                    System.out.println(maxSumNotSkip);
+                    break;
+                }else if (midSkip<maxSumBySkip){
+                    maxSumBySkip=midSkip;
+                    flag=true;
+                }
+            }
+            if (flag){
+                System.out.println(maxSumBySkip);
+            }
+        }
+
+    }
+}
