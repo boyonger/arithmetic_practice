6a2e1e5a2a33a7f62a6124096caa1e2263788f19
boyong.xu <boyong.xu@vipshop.com>
2018-06-13 19:17:26 +0800

    detele number not finish

---
+++ src/deleteNumber/DeleteNumber.java
@@ -0,0 +1,28 @@
+package deleteNumber;
+
+import java.io.IOException;
+
+class LinkNote{
+    private int data;
+    private LinkNote next;
+    LinkNote(){
+
+    }
+    LinkNote(int data){
+        this.data=data;
+    }
+}
+
+class Link{
+    LinkNote firstLink;
+     Link(LinkNote p){
+         firstLink =p;
+     }
+
+}
+
+public class DeleteNumber {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
--- src/dynamicPrograme/Palindrome.java
+++ src/dynamicPrograme/Palindrome.java
@@ -1,4 +1,4 @@
-package palindrome;
+package dynamicPrograme;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,19 +10,19 @@ public class Palindrome {
         String palindrome;
         while ((palindrome = bf.readLine()) != null) {
             int length = palindrome.length();
-            int[][] dynamicDis = new int[length+1][length+1];
+            int[][] dynamicDis = new int[length + 1][length + 1];
             for (int i = 0; i <= length; i++) {
                 for (int j = 0; j <= length; j++) {
                     if (i == 0 || j == 0) {
                         dynamicDis[i][j] = 0;
-                    } else if (palindrome.charAt(i-1) == palindrome.charAt(length - j)) {
+                    } else if (palindrome.charAt(i - 1) == palindrome.charAt(length - j)) {
                         dynamicDis[i][j] = dynamicDis[i - 1][j - 1] + 1;
                     } else {
                         dynamicDis[i][j] = Math.max(dynamicDis[i - 1][j], dynamicDis[i][j - 1]);
                     }
                 }
             }
-            if (dynamicDis[length - 1][length - 1] >= length - 1) {
+            if (dynamicDis[length][length] >= length - 1) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
