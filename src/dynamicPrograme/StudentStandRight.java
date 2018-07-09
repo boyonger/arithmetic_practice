b1a86d14e00a4a7068100021a37bbc3e865b3608
boyonger <32034024+boyonger@users.noreply.github.com>
2018-07-10 00:36:52 +0800

    3.0

--- src/dynamicPrograme/StudentStandRight.java
+++ src/dynamicPrograme/StudentStandRight.java
@@ -1,5 +1,9 @@
 package dynamicPrograme;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
 题目描述
 有n个学生站成一排，每个学生有一个能力值，牛牛想从这 n 个学生中按照顺序选取 k 名学生，要求相邻两个学生的位置编号的差不超过 d，
@@ -21,5 +25,57 @@ package dynamicPrograme;
 3 31
  */
 public class StudentStandRight {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer n = Integer.parseInt(br.readLine());
+        int[] ai = new int[n];
+        String[] str = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            ai[i] = Integer.parseInt(str[i]);
+        }
+        str = br.readLine().split(" ");
+        Integer k = Integer.parseInt(str[0]);
+        Integer d = Integer.parseInt(str[1]);
 
+        long[][] max = new long[k][n];
+        long[][] min = new long[k][n];
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    max[i][j] = ai[j];
+                    min[i][j] = ai[j];
+                } else {
+                    min[i][j] = 1;
+                    max[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 1; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int m = 1; m <= d; m++) {
+                    if (j - m >= 0) {
+                        if (ai[j] > 0) {
+                            max[i][j] = Math.max(max[i][j], max[i - 1][j - m] * ai[j]);
+                            min[i][j] = Math.min(min[i][j], min[i - 1][j - m] * ai[j]);
+                        } else {
+                            max[i][j] = Math.max(max[i][j], min[i - 1][j - m] * ai[j]);
+                            min[i][j] = Math.min(min[i][j], max[i - 1][j - m] * ai[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            if (i >= k - 1) {
+                res = Math.max(res, max[k - 1][i]);
+            }
+        }
+        System.out.println(res);
+
+
+    }
 }
