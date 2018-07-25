519bf0c9050772245ccb0fd00c664979203636fa
boyonger <32034024+boyonger@users.noreply.github.com>
2018-07-26 00:40:27 +0800

    9.0

--- src/graph/GraphMatrix.java
+++ src/graph/GraphMatrix.java
@@ -7,6 +7,14 @@ public class GraphMatrix {
 
     public GraphMatrix(int verNumber) {
         graphMatrix = new int[verNumber][verNumber];
+        for (int i = 0; i < graphMatrix.length; i++) {
+            for (int j = 0; j < graphMatrix[0].length; j++) {
+                if (i == j) graphMatrix[i][j] = 0
+                else{
+                    graphMatrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
         vertex = new int[verNumber];
     }
 
@@ -35,11 +43,32 @@ public class GraphMatrix {
     }
 
     public int getNextVertex(int v1, int v2) {
-        for (int i = v2+1; i < this.graphMatrix.length; i++) {
+        for (int i = v2 + 1; i < this.graphMatrix.length; i++) {
             if (graphMatrix[v1][i] > 0)
                 return i;
         }
         return -1;
+    }
+
+    public int[] distanceMin(int v) {
+        int vertexNumber = this.vertex.length;
+        int dis[] = new int[vertexNumber];
+        int vIn[] = new int[vertexNumber];
+        vIn[v] = 1;
+        dis[v] = 0;
+        for (int i = 0; i < dis.length; i++) {
+            dis[i] = graphMatrix[v][i];
+        }
+        for (int j = 0; j < vertexNumber; j++) {
+            if (dis[j] < Integer.MAX_VALUE) {
+                if (vIn[j] == 0) {
+                    vIn[j] = 1;
+                    for (int t = 0; t < vertexNumber; t++) {
+
+                    }
+                }
+            }
+        }
     }
 }
 
