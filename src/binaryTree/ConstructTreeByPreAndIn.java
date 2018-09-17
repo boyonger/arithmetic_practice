9481621a200ebb97e3bc2e77695f1a2d59a0c528
boyonger <32034024+boyonger@users.noreply.github.com>
2018-09-17 18:03:49 +0800

    order

--- src/binaryTree/ConstructTreeByPreAndIn.java
+++ src/binaryTree/ConstructTreeByPreAndIn.java
@@ -1,7 +1,5 @@
 package binaryTree;
 
-import java.util.Stack;
-
 class BinaryNode {
     public int value;
     public BinaryNode leftChild;
@@ -19,24 +17,45 @@ class BinaryTree {
     }
 
 
-    public void insertTree(BinaryNode root, int[] pre, int[] in, int prePos, int start, int end) {
-        root = new BinaryNode(pre[prePos]);
-        boolean flag = false;
-        for (int i = start; i < end; i++) {
+    public BinaryNode insertTreeByPreAndIn(int[] pre, int[] in, int prePos, int start, int end) {
+        if (prePos > pre.length - 1 || start > end) return null;
+        BinaryNode node = new BinaryNode(pre[prePos]);
+        int i;
+        for (i = start; i <= end; i++) {
             if (in[i] == pre[prePos]) {
-                flag = true;
-                root.leftChild = p;
-                insertTree(root.leftChild, in, pre, prePos++, start, i);
-                insertTree(root.leftChild, in, pre, prePos++, i + 1, end);
+                break;
+                /*在java中这么写会指向null
+                insertTreeByPreAndIn(root.leftChild, in, pre, prePos++, start, i);
+                insertTreeByPreAndIn(root.rightChild, in, pre, prePos++, i + 1, end);*/
             }
         }
-        if (flag == false) {
-            for (int i = inPos + 1; i < end; i++) {
-                if (in[i] == pre[prePos]) {
-                    root.rightChild = p;
-                    insertTree(root.leftChild, in, pre, prePos++, start, i, end);
-                }
-            }
+        node.leftChild = insertTreeByPreAndIn(in, pre, prePos + 1, start, i - 1);
+        node.rightChild = insertTreeByPreAndIn(in, pre, prePos + i - start + 1, i + 1, end);
+        return node;
+    }
+
+    //递归
+    public void preOrder(BinaryNode root) {
+        if (root != null) {
+            System.out.println(root.value);
+            preOrder(root.leftChild);
+            preOrder(root.rightChild);
+        }
+    }
+
+    public void inOrder(BinaryNode root) {
+        if (root != null) {
+            inOrder(root.leftChild);
+            System.out.println(root.value);
+            inOrder(root.rightChild);
+        }
+    }
+
+    public void backOrder(BinaryNode root) {
+        if (root != null) {
+            backOrder(root.leftChild);
+            backOrder(root.rightChild);
+            System.out.println(root.value);
         }
     }
 }
@@ -46,6 +65,7 @@ public class ConstructTreeByPreAndIn {
         int[] a = {1, 2, 4, 7, 3, 5, 6, 8};
         int[] b = {4, 7, 2, 1, 5, 3, 8, 6};
         BinaryTree binaryTree = new BinaryTree();
-        binaryTree.insertTree(binaryTree.root, a, b, 0, 0, b.length - 1);
+        binaryTree.root = binaryTree.insertTreeByPreAndIn(a, b, 0, 0, b.length - 1);
+        binaryTree.preOrder(binaryTree.root);
     }
 }
