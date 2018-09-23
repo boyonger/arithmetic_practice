8c53785c1f18ef515d609aa67a08d67df927f84e
boyonger <32034024+boyonger@users.noreply.github.com>
2018-09-23 11:52:56 +0800

    add singleton

--- src/javaBase/Singleton.java
+++ src/javaBase/Singleton.java
@@ -1,6 +1,38 @@
 package javaBase;
 
+class Student {
+    private String name;
+    private Long schoolId;
+
+    public Student() {
+        this.name = "yong";
+        this.schoolId = 20152100188l;
+    }
+}
+
 public class Singleton {
+    private static Student student;
+
+    private Singleton() {
+
+    }
+
+    public Student getInstance() {
+        if (student!=null){
+            return student;
+        }
+        synchronized {
+
+        }
+        if (student == null) {
+            synchronized (student) {
+                student = new Student();
+                return student;
+            }
+        } else {
+            return student;
+        }
+    }
 
 }
 
